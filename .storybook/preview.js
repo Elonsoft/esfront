fca782d5f3b21749afe0c3575a93ba6d680c63e5
Denis Yakshov <feerzlay@gmail.com>
2021-05-19 16:19:54 +0300

    feat: add locale switch

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import React, { useMemo } from 'react';
+
 import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { enUS, ruRU } from '@material-ui/core/locale';
+
 import { useDarkMode } from 'storybook-dark-mode';
 
 import { themeLight, themeDark } from './themes';
@@ -19,17 +22,28 @@ export const parameters = {
 };
 
 export const decorators = [
-  (Story) => {
+  (Story, context) => {
+    const locale = context.globals.locale;
     const isDarkMode = useDarkMode();
 
     const theme = useMemo(
       () =>
-        createTheme({
-          palette: {
-            mode: isDarkMode ? 'dark' : 'light'
-          }
-        }),
-      [isDarkMode]
+        createTheme(
+          {
+            palette: {
+              mode: isDarkMode ? 'dark' : 'light'
+            },
+            components: {
+              MuiButton: {
+                defaultProps: {
+                  disableElevation: true
+                }
+              }
+            }
+          },
+          locale === 'ru' ? ruRU : enUS
+        ),
+      [isDarkMode, locale]
     );
 
     return (
@@ -39,3 +53,20 @@ export const decorators = [
     );
   }
 ];
+
+const isRussian = window.navigator.language === 'ru-RU';
+
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Set the locale of the components',
+    defaultValue: isRussian ? 'ru' : 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', title: 'English' },
+        { value: 'ru', title: 'Русский' }
+      ]
+    }
+  }
+};
