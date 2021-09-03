7f047009e4f739fae7a73cbd638461952e203a8a
Denis Yakshov <feerzlay@gmail.com>
2021-09-03 15:47:56 +0300

    test: replace ts-jest with babel-jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  setupFilesAfterEnv: ['./jest.setup.ts'],
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['/node_modules/(?!(@babel/runtime|@mui/material))']
+  setupFilesAfterEnv: ['./jest.setup.ts']
 };
