449146b5c9e67e41f0aaf5e63a5b937d4a86e9a3
Denis Yakshov <feerzlay@gmail.com>
2021-12-29 13:31:03 +0300

    feat(useWindowEventListener): add new hook

--- src/ui/hooks/index.ts
+++ src/ui/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useDocumentEventListener } from './useDocumentEventListener';
 export { useLatest } from './useLatest';
 export { useResizeObserver } from './useResizeObserver';
+export { useWindowEventListener } from './useWindowEventListener';
---
+++ src/ui/hooks/useWindowEventListener/useWindowEventListener.stories.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import { Story } from '@storybook/react';
+
+import Typography from '@mui/material/Typography';
+
+import { useWindowEventListener } from './useWindowEventListener';
+
+export const Demo: Story = () => {
+  const [count, setCount] = useState(0);
+
+  useWindowEventListener('click', () => {
+    setCount(count + 1);
+  });
+
+  return <Typography variant="body100">Count of clicks on this page: {count}</Typography>;
+};
