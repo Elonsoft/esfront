716e63dbb75ad86dc2769bddac553c02b9694554
Denis Yakshov <feerzlay@gmail.com>
2021-12-29 16:33:03 +0300

    feat!: rename surface palette hues

--- src/theming/palettes/light/surface.ts
+++ src/theming/palettes/light/surface.ts
@@ -1,13 +1,13 @@
 export const surface = {
-  0: '#fff',
-  1: '#fff',
-  2: '#fff',
-  3: '#fff',
-  4: '#fff',
-  6: '#fff',
-  8: '#fff',
-  12: '#fff',
-  16: '#fff',
-  24: '#fff',
+  50: '#fff',
+  100: '#fff',
+  200: '#fff',
+  300: '#fff',
+  400: '#fff',
+  500: '#fff',
+  600: '#fff',
+  700: '#fff',
+  800: '#fff',
+  900: '#fff',
   on: '#f5f7fa'
 };
