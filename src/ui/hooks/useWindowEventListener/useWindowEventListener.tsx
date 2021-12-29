a6406211e3dcf4762fc00847bc7a391cfa578b40
Denis Yakshov <feerzlay@gmail.com>
2021-12-29 13:31:03 +0300

    feat(useWindowEventListener): add new hook

---
+++ src/ui/hooks/useWindowEventListener/useWindowEventListener.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+import { useLatest } from '../useLatest';
+
+/**
+ * @param type A case-sensitive string representing the event type to listen for.
+ * @param callback The function that receives a notification when an event of the specified type occurs.
+ * @param options An object that specifies characteristics about the event listener.
+ */
+export const useWindowEventListener = <K extends keyof WindowEventMap>(
+  type: K,
+  callback: (event: WindowEventMap[K]) => any,
+  options?: boolean | AddEventListenerOptions
+) => {
+  const latestCallback = useLatest(callback);
+
+  useEffect(() => {
+    const onEvent = (event: WindowEventMap[K]) => {
+      return latestCallback.current(event);
+    };
+
+    window.addEventListener(type, onEvent, options);
+
+    return () => {
+      window.removeEventListener(type, onEvent);
+    };
+  }, []);
+};
