406a20f53f891c40a92f9720458ba750dd25ae79
Vadim Nesterenko <vanst25j@gmail.com>
2022-01-13 19:40:56 +0300

    feat(Snackbar): add new component
    Co-authored-by: Denis Yakshov <feerzlay@gmail.com>

---
+++ packages/react/src/components/Snackbar/Snackbar.classes.ts
@@ -0,0 +1,79 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/material';
+
+export type SnackbarClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+  /** Styles applied to the progress element. */
+  progress: string;
+  /** Styles applied to the content element. */
+  content: string;
+  /** Styles applied to the icon element. */
+  icon: string;
+  /** Styles applied to the message element. */
+  message: string;
+  /** Styles applied to the actions element. */
+  actions: string;
+
+  /** Styles applied to the root element if severity="success". */
+  severitySuccess: string;
+  /** Styles applied to the root element if severity="warning". */
+  severityWarning: string;
+  /** Styles applied to the root element if severity="error". */
+  severityError: string;
+  /** Styles applied to the root element if severity="info". */
+  severityInfo: string;
+  /** Styles applied to the root element if severity="default". */
+  severityDefault: string;
+
+  /** Styles applied to the root element if severity="success" or color="success". */
+  colorSuccess: string;
+  /** Styles applied to the root element if severity="warning" or color="warning". */
+  colorWarning: string;
+  /** Styles applied to the root element if severity="error" or color="error". */
+  colorError: string;
+  /** Styles applied to the root element if severity="info" or color="info". */
+  colorInfo: string;
+  /** Styles applied to the root element if severity="default" or color="monoA". */
+  colorMonoA: string;
+
+  /** Styles applied to the root element if size="s". */
+  sizeS: string;
+  /** Styles applied to the root element if size="m". */
+  sizeM: string;
+  /** Styles applied to the root element if size="l". */
+  sizeL: string;
+  /** Styles applied to the root element if size="xl". */
+  sizeXl: string;
+};
+
+export type SnackbarClassKey = keyof SnackbarClasses;
+
+export function getSnackbarUtilityClass(slot: string): string {
+  return generateUtilityClass('ESSnackbar', slot);
+}
+
+export const snackbarClasses: SnackbarClasses = generateUtilityClasses('ESSnackbar', [
+  'root',
+  'progress',
+  'content',
+  'icon',
+  'message',
+  'actions',
+
+  'severitySuccess',
+  'severityWarning',
+  'severityError',
+  'severityInfo',
+  'severityDefault',
+
+  'colorSuccess',
+  'colorWarning',
+  'colorError',
+  'colorInfo',
+  'colorMonoA',
+
+  'sizeS',
+  'sizeM',
+  'sizeL',
+  'sizeXl',
+]);
---
+++ packages/react/src/components/Snackbar/index.ts
@@ -0,0 +1,12 @@
+export { Snackbar } from './Snackbar';
+export { SnackbarClasses, snackbarClasses, SnackbarClassKey } from './Snackbar.classes';
+export {
+  SnackbarProps,
+  SnackbarPropsColor,
+  SnackbarPropsColorOverrides,
+  SnackbarPropsSeverity,
+  SnackbarPropsSeverityOverrides,
+  SnackbarPropsSize,
+  SnackbarPropsSizeOverrides,
+} from './Snackbar.types';
+export * from './SnackbarClose';
