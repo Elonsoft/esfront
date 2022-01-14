ac75b6e33c0704d67a6d7d3b9c632aaa7c0488e1
Denis Yakshov <feerzlay@gmail.com>
2022-01-14 14:39:48 +0300

    feat: add shadow palette

---
+++ src/theming/palettes/dark/shadow.ts
@@ -0,0 +1,50 @@
+export const shadow = {
+  down: {
+    50: '0px 1px 0px rgba(0, 0, 0, 0.12)',
+    100: '0.5px -0.5px 2px 1px rgba(0, 0, 0, 0.08), -1px 0px 2px rgba(0, 0, 0, 0.28)',
+    200: '0px -0.5px 1px 1px rgba(0, 0, 0, 0.08), 0px 1px 0px rgba(0, 0, 0, 0.08), 0px 2px 4px rgba(0, 0, 0, 0.24)',
+    300: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 1px 2px rgba(0, 0, 0, 0.12), 0px 3px 6px rgba(0, 0, 0, 0.24)',
+    400: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 1px 2px rgba(0, 0, 0, 0.16), 0px 4px 8px rgba(0, 0, 0, 0.24)',
+    500: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 2px 4px rgba(0, 0, 0, 0.08), 0px 6px 12px rgba(0, 0, 0, 0.28)',
+    600: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 3px 6px rgba(0, 0, 0, 0.12), 0px 8px 16px rgba(0, 0, 0, 0.28)',
+    700: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 4px 8px rgba(0, 0, 0, 0.12), 0px 12px 24px rgba(0, 0, 0, 0.32)',
+    800: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), 0px 5px 10px rgba(0, 0, 0, 0.12), 0px 16px 32px rgba(0, 0, 0, 0.36)',
+    900: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), 0px 6px 12px rgba(0, 0, 0, 0.12), 0px 24px 48px rgba(0, 0, 0, 0.48)'
+  },
+  up: {
+    50: '0px -1px 0px rgba(0, 0, 0, 0.12)',
+    100: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px -1px 2px rgba(0, 0, 0, 0.28)',
+    200: '0px 0.5px 1px 1px rgba(0, 0, 0, 0.08), 0px -1px 0px rgba(0, 0, 0, 0.08), 0px -2px 4px rgba(0, 0, 0, 0.24)',
+    300: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px -1px 2px rgba(0, 0, 0, 0.12), 0px -3px 6px rgba(0, 0, 0, 0.24)',
+    400: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px -1px 2px rgba(0, 0, 0, 0.16), 0px -4px 8px rgba(0, 0, 0, 0.24)',
+    500: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px -2px 4px rgba(0, 0, 0, 0.08), 0px -6px 12px rgba(0, 0, 0, 0.28)',
+    600: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px -3px 6px rgba(0, 0, 0, 0.12), 0px -8px 16px rgba(0, 0, 0, 0.28)',
+    700: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px -4px 8px rgba(0, 0, 0, 0.12), 0px -12px 24px rgba(0, 0, 0, 0.32)',
+    800: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), 0px -5px 10px rgba(0, 0, 0, 0.12), 0px -16px 32px rgba(0, 0, 0, 0.36)',
+    900: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), 0px -6px 12px rgba(0, 0, 0, 0.12), 0px -24px 48px rgba(0, 0, 0, 0.48)'
+  },
+  left: {
+    50: '-1px 0px 0px rgba(0, 0, 0, 0.12)',
+    100: '0.5px -0.5px 2px 1px rgba(0, 0, 0, 0.08), -1px 0px 2px rgba(0, 0, 0, 0.28)',
+    200: '0.5px 0px 1px 1px rgba(0, 0, 0, 0.08), -1px 0px 0px rgba(0, 0, 0, 0.08), -2px 0px 4px rgba(0, 0, 0, 0.24)',
+    300: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), -1px 0px 2px rgba(0, 0, 0, 0.12), -3px 0px 6px rgba(0, 0, 0, 0.24)',
+    400: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), -1px 0px 2px rgba(0, 0, 0, 0.16), -4px 0px 8px rgba(0, 0, 0, 0.24)',
+    500: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), -2px 0px 4px rgba(0, 0, 0, 0.08), -6px 0px 12px rgba(0, 0, 0, 0.28)',
+    600: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), -3px 0px 6px rgba(0, 0, 0, 0.12), -8px 0px 16px rgba(0, 0, 0, 0.28)',
+    700: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), -4px 0px 8px rgba(0, 0, 0, 0.12), -12px 0px 24px rgba(0, 0, 0, 0.32)',
+    800: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), -5px 0px 10px rgba(0, 0, 0, 0.12), -16px 0px 32px rgba(0, 0, 0, 0.36)',
+    900: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), -6px 0px 12px rgba(0, 0, 0, 0.12), -24px 0px 48px rgba(0, 0, 0, 0.48)'
+  },
+  right: {
+    50: '1px 0px 0px rgba(0, 0, 0, 0.12)',
+    100: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 1px 0px 2px rgba(0, 0, 0, 0.28)',
+    200: '-0.5px 0px 1px 1px rgba(0, 0, 0, 0.08), 1px 0px 0px rgba(0, 0, 0, 0.08), 2px 0px 4px rgba(0, 0, 0, 0.24)',
+    300: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 1px 0px 2px rgba(0, 0, 0, 0.12), 3px 0px 6px rgba(0, 0, 0, 0.24)',
+    400: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 1px 0px 2px rgba(0, 0, 0, 0.16), 4px 0px 8px rgba(0, 0, 0, 0.24)',
+    500: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 2px 0px 4px rgba(0, 0, 0, 0.08), 6px 0px 12px rgba(0, 0, 0, 0.28)',
+    600: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 3px 0px 6px rgba(0, 0, 0, 0.12), 8px 0px 16px rgba(0, 0, 0, 0.28)',
+    700: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 4px 0px 8px rgba(0, 0, 0, 0.12), 12px 0px 24px rgba(0, 0, 0, 0.32)',
+    800: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), 5px 0px 10px rgba(0, 0, 0, 0.12), 16px 0px 32px rgba(0, 0, 0, 0.36)',
+    900: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.08), 0px 0px 2px rgba(0, 0, 0, 0.08), 6px 0px 12px rgba(0, 0, 0, 0.12), 24px 0px 48px rgba(0, 0, 0, 0.48)'
+  }
+};
---
+++ src/theming/palettes/light/shadow.ts
@@ -0,0 +1,50 @@
+export const shadow = {
+  down: {
+    50: '0px 1px 0px rgba(0, 0, 0, 0.06)',
+    100: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 1px 2px rgba(0, 0, 0, 0.07)',
+    200: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 1px 0px rgba(0, 0, 0, 0.02), 0px 2px 4px rgba(0, 0, 0, 0.06)',
+    300: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 1px 2px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(0, 0, 0, 0.06)',
+    400: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 1px 2px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.06)',
+    500: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 2px 4px rgba(0, 0, 0, 0.02), 0px 6px 12px rgba(0, 0, 0, 0.07)',
+    600: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 3px 6px rgba(0, 0, 0, 0.03), 0px 8px 16px rgba(0, 0, 0, 0.07)',
+    700: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 4px 8px rgba(0, 0, 0, 0.03), 0px 12px 24px rgba(0, 0, 0, 0.08)',
+    800: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), 0px 5px 10px rgba(0, 0, 0, 0.03), 0px 16px 32px rgba(0, 0, 0, 0.09)',
+    900: '0px -0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), 0px 6px 12px rgba(0, 0, 0, 0.03), 0px 24px 48px rgba(0, 0, 0, 0.12)'
+  },
+  up: {
+    50: '0px -1px 0px rgba(0, 0, 0, 0.06)',
+    100: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -1px 2px rgba(0, 0, 0, 0.07)',
+    200: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -1px 0px rgba(0, 0, 0, 0.02), 0px -2px 4px rgba(0, 0, 0, 0.06)',
+    300: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -1px 2px rgba(0, 0, 0, 0.03), 0px -3px 6px rgba(0, 0, 0, 0.06)',
+    400: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -1px 2px rgba(0, 0, 0, 0.04), 0px -4px 8px rgba(0, 0, 0, 0.06)',
+    500: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -2px 4px rgba(0, 0, 0, 0.02), 0px -6px 12px rgba(0, 0, 0, 0.07)',
+    600: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -3px 6px rgba(0, 0, 0, 0.03), 0px -8px 16px rgba(0, 0, 0, 0.07)',
+    700: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px -4px 8px rgba(0, 0, 0, 0.03), 0px -12px 24px rgba(0, 0, 0, 0.08)',
+    800: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), 0px -5px 10px rgba(0, 0, 0, 0.03), 0px -16px 32px rgba(0, 0, 0, 0.09)',
+    900: '0px 0.5px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), 0px -6px 12px rgba(0, 0, 0, 0.03), 0px -24px 48px rgba(0, 0, 0, 0.12)'
+  },
+  left: {
+    50: '-1px 0px 0px rgba(0, 0, 0, 0.06)',
+    100: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -1px 0px 2px rgba(0, 0, 0, 0.07)',
+    200: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -1px 0px 0px rgba(0, 0, 0, 0.02), -2px 0px 4px rgba(0, 0, 0, 0.06)',
+    300: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -1px 0px 2px rgba(0, 0, 0, 0.03), -3px 0px 6px rgba(0, 0, 0, 0.06)',
+    400: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -1px 0px 2px rgba(0, 0, 0, 0.04), -4px 0px 8px rgba(0, 0, 0, 0.06)',
+    500: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -2px 0px 4px rgba(0, 0, 0, 0.02), -6px 0px 12px rgba(0, 0, 0, 0.07)',
+    600: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -3px 0px 6px rgba(0, 0, 0, 0.03), -8px 0px 16px rgba(0, 0, 0, 0.07)',
+    700: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), -4px 0px 8px rgba(0, 0, 0, 0.03), -12px 0px 24px rgba(0, 0, 0, 0.08)',
+    800: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), -5px 0px 10px rgba(0, 0, 0, 0.03), -16px 0px 32px rgba(0, 0, 0, 0.09)',
+    900: '0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), -6px 0px 12px rgba(0, 0, 0, 0.03), -24px 0px 48px rgba(0, 0, 0, 0.12)'
+  },
+  right: {
+    50: '1px 0px 0px rgba(0, 0, 0, 0.06)',
+    100: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 1px 0px 2px rgba(0, 0, 0, 0.07)',
+    200: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 1px 0px 0px rgba(0, 0, 0, 0.02), 2px 0px 4px rgba(0, 0, 0, 0.06)',
+    300: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 1px 0px 2px rgba(0, 0, 0, 0.03), 3px 0px 6px rgba(0, 0, 0, 0.06)',
+    400: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 1px 0px 2px rgba(0, 0, 0, 0.04), 4px 0px 8px rgba(0, 0, 0, 0.06)',
+    500: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 2px 0px 4px rgba(0, 0, 0, 0.02), 6px 0px 12px rgba(0, 0, 0, 0.07)',
+    600: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 3px 0px 6px rgba(0, 0, 0, 0.03), 8px 0px 16px rgba(0, 0, 0, 0.07)',
+    700: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 4px 0px 8px rgba(0, 0, 0, 0.03), 12px 0px 24px rgba(0, 0, 0, 0.08)',
+    800: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), 5px 0px 10px rgba(0, 0, 0, 0.03), 16px 0px 32px rgba(0, 0, 0, 0.09)',
+    900: '-0.5px 0px 2px 1px rgba(0, 0, 0, 0.02), 0px 0px 2px rgba(0, 0, 0, 0.02), 6px 0px 12px rgba(0, 0, 0, 0.03), 24px 0px 48px rgba(0, 0, 0, 0.12)'
+  }
+};
