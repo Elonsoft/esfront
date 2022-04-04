5306c66e209b40749e644e1049722dbf686f7c40
Andrey Medvedev <medveddevaa@icloud.com>
2022-04-04 13:05:33 +0300

    feat: add missing hues to info, success and warning palettes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test', 'chore']]
+    'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test', 'chore']],
+    'header-max-length': [0, 'always']
   }
 };
