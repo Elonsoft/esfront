177ebcf4d203e425ff68dc287251f0cd3e5510b3
Andrey Medvedev <medveddevaa@icloud.com>
2022-04-08 14:22:54 +0300

    feat: change alpha, surface and background hues

--- src/theming/palettes/dark/common.ts
+++ src/theming/palettes/dark/common.ts
@@ -1,6 +1,6 @@
 export const common = {
-  background: '#18181a',
-  background2: '#09090a',
+  background: '#1C1C1F',
+  background2: '#111314',
   link: '#62a7ff',
   linkVisited: '#be95ff',
   switch: '#18181A'
--- src/theming/palettes/light/common.ts
+++ src/theming/palettes/light/common.ts
@@ -1,6 +1,6 @@
 export const common = {
-  background: '#f5f7fa',
-  background2: '#edf0f5',
+  background: '#F5F7FA',
+  background2: '#EDF0F5',
   link: '#1c75e7',
   linkVisited: '#883df9',
   switch: '#fafafa'
