aa068de7ee576e000159a8cf555c2ea65279ac7a
Denis Yakshov <feerzlay@gmail.com>
2022-05-20 16:45:57 +0300

    docs(useIntersectionObserver): add documentation

---
+++ src/hooks/useIntersectionObserver/useIntersectionObserver.stories.tsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from 'react';
+
+import { Story } from '@storybook/react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import { useIntersectionObserver } from './useIntersectionObserver';
+
+export const Demo: Story = () => {
+  const [isIntersecting, setIntesecting] = useState(false);
+  const ref = useRef<HTMLElement | null>(null);
+
+  useIntersectionObserver(ref, (entries) => {
+    setIntesecting(entries[0].isIntersecting);
+  });
+
+  return (
+    <Box
+      sx={{
+        height: 'min(calc(100vh - 32px), 300px)',
+        overflow: 'auto',
+        border: '1px solid black',
+        borderRadius: '4px'
+      }}
+    >
+      <Typography
+        sx={{ position: 'sticky', top: 0, backgroundColor: 'monoB.500', padding: '4px 8px' }}
+        component="div"
+        variant="body100"
+      >
+        Bottom element is intersecting: {isIntersecting.toString()}
+      </Typography>
+      <Box sx={{ height: '600px' }} />
+      <Box ref={ref} sx={{ height: '64px', backgroundColor: 'primary.300' }} />
+    </Box>
+  );
+};
--- src/hooks/useIntersectionObserver/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver/useIntersectionObserver.ts
@@ -2,6 +2,12 @@ import { RefObject, useEffect } from 'react';
 
 import { useLatest } from '../useLatest';
 
+/**
+ * The hook that provides a way to asynchronously observe changes in the intersection of a target element with an ancestor element or with a top-level document's viewport.
+ * @param element A reference to an Element to be observed.
+ * @param callback The function called when the percentage of the target element is visible crosses a threshold.
+ * @param options An options object allowing you to set options for the observation.
+ */
 export const useIntersectionObserver = (
   element: RefObject<Element>,
   callback: (entries: IntersectionObserverEntry[]) => void,
