Elonsoft/elonkit-react

feat(Rating): add style overrides

src/theming/components/rating/rating.stories.tsx
@@ -1,0 +1,11 @@
+import { useState } from 'react';
+
+import { Story } from '@storybook/react';
+
+import Rating from '@mui/material/Rating';
+
+export const Demo: Story = ({ ...args }) => {
+  const [value, setValue] = useState<number | null>(3);
+
+  return <Rating value={value} onChange={(event, newValue) => setValue(newValue)} {...args} />;
+};

Elonsoft/esfront

src/theming/components/rating/rating.stories.tsx
@@ -1,0 +1,11 @@
+import { useState } from 'react';
+
+import { Story } from '@storybook/react';
+
+import Rating from '@mui/material/Rating';
+
+export const Demo: Story = ({ ...args }) => {
+  const [value, setValue] = useState<number | null>(3);
+
+  return <Rating value={value} onChange={(e, newValue) => setValue(newValue)} {...args} />;
+};