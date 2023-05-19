f54acdb3f3e435e1d2ffad8ef47c621d33863987
Vadim Nesterenko <vanst25j@gmail.com>
2023-05-19 12:04:27 +0300

    docs(useForceUpdate): add stable initial state

--- src/hooks/useForceUpdate/useForceUpdate.stories.tsx
+++ src/hooks/useForceUpdate/useForceUpdate.stories.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import { Story } from '@storybook/react';
 
 import Button from '@mui/material/Button';
@@ -8,13 +10,22 @@ import { useForceUpdate } from './useForceUpdate';
 export const Demo: Story = () => {
   const update = useForceUpdate();
 
+  const date = useRef(new Date(0).getTime());
+  const isFirst = useRef(true);
+
+  if (isFirst.current) {
+    isFirst.current = false;
+  } else {
+    date.current = new Date().getTime();
+  }
+
   return (
     <>
       <Button variant="contained" onClick={update} size="32">
         Update
       </Button>
       <Typography component="div" variant="body200" sx={{ marginTop: '8px' }}>
-        Time: {Date.now()}
+        Time: {date.current}
       </Typography>
     </>
   );
