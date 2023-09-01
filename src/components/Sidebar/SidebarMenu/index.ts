c80878e9ccb193d5c117f85459e7962bb44e36cf
Denis Yakshov <feerzlay@gmail.com>
2023-09-01 14:58:25 +0300

    fix(Sidebar): add missing context exports

--- src/components/Sidebar/SidebarMenu/index.ts
+++ src/components/Sidebar/SidebarMenu/index.ts
@@ -1,3 +1,4 @@
 export { SidebarMenu } from './SidebarMenu';
 export { SidebarMenuClasses, sidebarMenuClasses, SidebarMenuClassKey } from './SidebarMenu.classes';
+export { SidebarMenuContext, SidebarMenuContextValue, useSidebarMenuContext } from './SidebarMenu.context';
 export { SidebarMenuProps } from './SidebarMenu.types';
