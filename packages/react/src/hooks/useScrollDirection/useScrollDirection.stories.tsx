9cf89808c0025dee8e5f42ff9bb13b2747ca6f11
Svyatoslav <pqrupq@gmail.com>
2024-04-25 15:52:54 +0300

    feat(useScrollDirection): add new hook

---
+++ packages/react/src/hooks/useScrollDirection/useScrollDirection.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+
+import { useScrollDirection } from './useScrollDirection';
+
+type Args = { throttleTimeout?: number };
+
+const meta: Meta = {
+  tags: ['autodocs'],
+  title: 'Hooks/useScrollDirection',
+  parameters: {
+    references: ['useScrollDirection'],
+    docs: {
+      story: { inline: false },
+    },
+  },
+  argTypes: {
+    throttleTimeout: {
+      control: {
+        type: 'number',
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<Args>;
+
+export const Demo: Story = {
+  render: function Render(args) {
+    const scrollDirection = useScrollDirection({ throttleTimeout: args.throttleTimeout });
+
+    return (
+      <>
+        <Typography sx={{ position: 'fixed' }} variant="overline">
+          {scrollDirection ? scrollDirection : `the page hasn't been scrolled`}
+        </Typography>
+        <Box sx={{ height: '150vh' }} />
+      </>
+    );
+  },
+};
