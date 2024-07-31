1beff424acc8f7a5c84be2790e1960dc19465c86
e.timofeeva@elonsoft.ru <e.timofeeva@elonsoft.ru>
2024-07-31 21:20:04 +0300

    feat(theme,kbd): add new variants

--- packages/react/src/components/Kbd/Kbd.tsx
+++ packages/react/src/components/Kbd/Kbd.tsx
@@ -11,7 +11,14 @@ export const Kbd = (inProps: KbdProps) => {
   const { children, className, style, variant = 'raised' } = useDefaultProps({ props: inProps, name: 'ESKbd' });
 
   return (
-    <kbd className={clsx(`es-kbd es-kbd--${variant}`, 'body100', className)} style={style}>
+    <kbd
+      className={clsx(
+        `es-kbd es-kbd--${variant}`,
+        variant === 'mono-a' || variant === 'mono-b' ? 'caption' : 'body100',
+        className
+      )}
+      style={style}
+    >
       {children}
     </kbd>
   );
