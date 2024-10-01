Elonsoft/elonkit-react

feat(PhoneField): add new component

src/components/PhoneField/PhoneField.classes.ts
@@ -1,0 +1,27 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
+
+export type PhoneFieldClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+  startAdornment: string;
+  menuButton: string;
+  item: string;
+  itemPrimary: string;
+  countryDisplayName: string;
+  countryCallingCode: string;
+};
+export type PhoneFieldClassKey = keyof PhoneFieldClasses;
+
+export function getPhoneFieldUtilityClass(slot: string): string {
+  return generateUtilityClass('ESPhoneField', slot);
+}
+
+export const phoneFieldClasses: PhoneFieldClasses = generateUtilityClasses('ESPhoneField', [
+  'root',
+  'startAdornment',
+  'menuButton',
+  'item',
+  'itemPrimary',
+  'countryDisplayName',
+  'countryCallingCode'
+]);

Elonsoft/esfront

packages/react/src/components/PhoneField/PhoneField.classes.ts
@@ -1,0 +1,27 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/material';
+
+export type PhoneFieldClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+  startAdornment: string;
+  menuButton: string;
+  item: string;
+  itemPrimary: string;
+  countryDisplayName: string;
+  countryCallingCode: string;
+};
+export type PhoneFieldClassKey = keyof PhoneFieldClasses;
+
+export function getPhoneFieldUtilityClass(slot: string): string {
+  return generateUtilityClass('ESPhoneField', slot);
+}
+
+export const phoneFieldClasses: PhoneFieldClasses = generateUtilityClasses('ESPhoneField', [
+  'root',
+  'startAdornment',
+  'menuButton',
+  'item',
+  'itemPrimary',
+  'countryDisplayName',
+  'countryCallingCode',
+]);