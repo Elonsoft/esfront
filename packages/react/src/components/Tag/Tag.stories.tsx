ebf86e7dbc2b9d047d5b2bce7b0e2f31ef989928
Vadim Nesterenko <vanst25j@gmail.com>
2024-10-02 12:53:38 +0300

    feat(Tag): add new component

---
+++ packages/react/src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,74 @@
+import { ComponentProps } from 'react';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Tag } from './Tag';
+
+import { IconInformationFillW200 } from '../../icons';
+
+type Args = ComponentProps<typeof Tag> & { component?: unknown };
+
+const meta: Meta<Args> = {
+  tags: ['autodocs'],
+  component: Tag,
+  parameters: {
+    references: ['Tag'],
+  },
+  argTypes: {
+    component: {
+      table: {
+        disable: true,
+      },
+    },
+    startIcon: {
+      table: {
+        disable: true,
+      },
+    },
+    endIcon: {
+      table: {
+        disable: true,
+      },
+    },
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+    variant: {
+      options: ['overline', 'caption'],
+      control: { type: 'select' },
+    },
+    color: {
+      options: ['success', 'warning', 'error', 'info', 'secondary', 'primary', 'monoA', 'monoB'],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+  args: {
+    children: 'Tag',
+    color: 'primary',
+    variant: 'overline',
+  },
+};
+
+export default meta;
+type Story = StoryObj<Args>;
+
+export const Demo: Story = {
+  render: (args) => {
+    return (
+      <div style={{ display: 'flex', gap: '16px' }}>
+        <Tag
+          endIcon={<IconInformationFillW200 container containerSize="16px" />}
+          startIcon={<IconInformationFillW200 container containerSize="16px" />}
+          {...args}
+        />
+        <Tag endIcon={<IconInformationFillW200 container containerSize="16px" />} {...args} />
+        <Tag startIcon={<IconInformationFillW200 container containerSize="16px" />} {...args} />
+        <Tag {...args} />
+      </div>
+    );
+  },
+};
