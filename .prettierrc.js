Elonsoft/elonkit-react

style: upgrade eslint to v9

.prettierrc.js
@@ -1,1 +1,1 @@
-module.exports = require('@elonkit/prettier-config');
+export { default } from '@elonkit/prettier-config';

Elonsoft/esfront

.prettierrc.js
@@ -1,1 +1,1 @@
-module.exports = require('@esfront/prettier-config');
+export { default } from '@esfront/prettier-config';