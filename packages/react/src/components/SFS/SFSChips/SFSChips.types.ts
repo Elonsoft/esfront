f5f55721ef5013e6e62d7d46236b92c90f1ebf85
Denis Yakshov <feerzlay@gmail.com>
2024-12-13 18:53:43 +0300

    feat(SFSChips): add new component

---
+++ packages/react/src/components/SFS/SFSChips/SFSChips.types.ts
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+import { SFSChipsClasses } from './SFSChips.classes';
+
+import { SxProps, Theme } from '@mui/material';
+
+export interface SFSChipsProps {
+  children?: ReactNode;
+  /** Override or extend the styles applied to the component. */
+  classes?: Partial<SFSChipsClasses>;
+  /** The system prop that allows defining system overrides as well as additional CSS styles. */
+  sx?: SxProps<Theme>;
+  /** Class applied to the root element. */
+  className?: string;
+
+  /** Text for the delete button label. */
+  labelDelete?: string;
+  /** Icon for the delete button. */
+  iconDelete?: ReactNode;
+  /** Callback fired when the delete button is clicked.*/
+  onDelete?: () => void;
+}
