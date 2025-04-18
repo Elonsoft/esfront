Elonsoft/elonkit-react

feat(Auth): add new component

src/components/Auth/AuthLink/AuthLink.classes.ts
@@ -1,0 +1,13 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
+
+export type AuthLinkClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+};
+export type AuthLinkClassKey = keyof AuthLinkClasses;
+
+export function getAuthLinkUtilityClass(slot: string): string {
+  return generateUtilityClass('ESAuthLink', slot);
+}
+
+export const authLinkClasses: AuthLinkClasses = generateUtilityClasses('ESAuthLink', ['root']);

src/components/Auth/AuthLogo/AuthLogo.classes.ts
@@ -1,0 +1,13 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/base';
+
+export type AuthLogoClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+};
+export type AuthLogoClassKey = keyof AuthLogoClasses;
+
+export function getAuthLogoUtilityClass(slot: string): string {
+  return generateUtilityClass('ESAuthLogo', slot);
+}
+
+export const authLogoClasses: AuthLogoClasses = generateUtilityClasses('ESAuthLogo', ['root']);

Elonsoft/esfront

packages/react/src/components/Auth/AuthLink/AuthLink.classes.ts
@@ -1,0 +1,13 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/material';
+
+export type AuthLinkClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+};
+export type AuthLinkClassKey = keyof AuthLinkClasses;
+
+export function getAuthLinkUtilityClass(slot: string): string {
+  return generateUtilityClass('ESAuthLink', slot);
+}
+
+export const authLinkClasses: AuthLinkClasses = generateUtilityClasses('ESAuthLink', ['root']);

packages/react/src/components/Auth/AuthLogo/AuthLogo.classes.ts
@@ -1,0 +1,13 @@
+import { generateUtilityClass, generateUtilityClasses } from '@mui/material';
+
+export type AuthLogoClasses = {
+  /** Styles applied to the root element. */
+  root: string;
+};
+export type AuthLogoClassKey = keyof AuthLogoClasses;
+
+export function getAuthLogoUtilityClass(slot: string): string {
+  return generateUtilityClass('ESAuthLogo', slot);
+}
+
+export const authLogoClasses: AuthLogoClasses = generateUtilityClasses('ESAuthLogo', ['root']);