f174a8460d9c2ed9f70fd238cf454f32c0e4a74f
Denis Yakshov <feerzlay@gmail.com>
2025-04-22 10:43:17 +0300

    feat(Price): add new component

---
+++ packages/react/src/components/Price/index.ts
@@ -0,0 +1,4 @@
+export { Price } from './Price';
+export type { PriceClasses, PriceClassKey } from './Price.classes';
+export { priceClasses } from './Price.classes';
+export type { PriceProps } from './Price.types';
