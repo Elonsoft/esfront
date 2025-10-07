affae2e6682f5df7272802d5eb89f69c87c1cc69
Denis Yakshov <feerzlay@gmail.com>
2025-10-07 16:53:42 +0300

    feat(Image): add new component

---
+++ packages/react/src/components/Image/index.ts
@@ -0,0 +1,4 @@
+export { Image } from './Image';
+export type { ImageClasses, ImageClassKey } from './Image.classes';
+export { imageClasses } from './Image.classes';
+export type { ImageImperativeActions, ImageProps } from './Image.types';
